a2faba84ed473f7400651a540f6f824417fb0fac
JeYeMC <juan.1701626316@ucaldas.edu.co>
2025-04-15 17:44:47 -0500

    Cambios en el main de pruebas

--- src/main.py
+++ src/main.py
@@ -1,23 +1,31 @@
 from src.controllers.manager import Manager
-
 from src.controllers.strategies.q_nodes import QNodes
-
+from src.controllers.strategies.phi import Phi
 
 def iniciar():
     """Punto de entrada principal"""
-                    # ABCD #
-    estado_inicial = "1000"
-    condiciones =    "1110"
-    alcance =        "1110"
-    mecanismo =      "1110"
+                    # ABCDEFGHIJ #
+    estado_inicial = "1000000000"
+    condiciones =    "1111111111"
+    alcance =        "0111111001"
+    mecanismo =      "0111111111"
 
     gestor_sistema = Manager(estado_inicial)
 
-    ### Ejemplo de solución mediante módulo de fuerza bruta ###
+    ### Ejemplo de solución mediante módulo Q-Nodes ###
     analizador_fb = QNodes(gestor_sistema)
     sia_uno = analizador_fb.aplicar_estrategia(
-        condiciones,
-        alcance,
-        mecanismo,
+         condiciones,
+         alcance,
+         mecanismo,
     )
     print(sia_uno)
+
+    ### Ejemplo de solución mediante módulo Phi ###
+    #analizador_fi = Phi(gestor_sistema)
+    #sia_dos = analizador_fi.aplicar_estrategia(
+    #    condiciones,
+    #    alcance,
+    #    mecanismo,
+    #)
+    #print(sia_dos)
